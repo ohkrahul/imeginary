33698b15db5b1a5435aecab8b7810a0a57641e57
Ohkrahull <rahul@beeyond.agency>
2024-12-22 20:06:50 +0530

    initial

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 
-import "./globals.css";
 
 const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
